dd9f59826b91ad0fce6bd29a89b9013a22e3ec39
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-05-17 21:25:36 -0300

    terminei outro componente

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import styles from './App.module.css'
 import Search from '../Search/search';
+import CurrentWeather from '../CurrentWeather/currentweather';
 
 function App() {
   return (
     <div className="App">
       <Search />
+      <CurrentWeather />
     </div>
   );
 }
