260d95b919d8728d1cf2222fe8120268087eedc9
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-05-18 12:53:58 -0300

    finalizei o css do ultimo componente

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import styles from './App.module.css'
 import Search from '../Search/search';
 import CurrentWeather from '../CurrentWeather/currentweather';
+import Forecast from '../Forecast/forecast';
 
 function App() {
   return (
     <div className="App">
       <Search />
       <CurrentWeather />
+      <Forecast />
     </div>
   );
 }
--- src/components/Forecast/forecast.js
+++ src/components/Forecast/forecast.js
@@ -1,1 +1,42 @@
 import React from "react";
+import styles from './forecast.module.css';
+import imageSun from '../sun.png';
+import imageStorm from '../storm.png';
+import imageCloudyandrain from '../cloudyandraining.png';
+import imageCloudy from '../cloudy.png';
+
+function Forecast() {
+    
+    return (
+        <div className={styles.container}>
+            <section className={styles.forecastContainer}>
+                <section className={styles.forecastModule}>
+                    <img className={styles.weatherImage} src={imageSun}></img>
+                    <p className={styles.hourandtemp}>01:00 pm</p>
+                    <p className={styles.hourandtemp}>21º</p>
+                </section>
+
+                <section className={styles.forecastModule}>
+                    <img className={styles.weatherImage} src={imageStorm}></img>
+                    <p className={styles.hourandtemp}>01:00 pm</p>
+                    <p className={styles.hourandtemp}>21º</p>
+                </section>
+
+                <section className={styles.forecastModule}>
+                    <img className={styles.weatherImage} src={imageCloudyandrain}></img>
+                    <p className={styles.hourandtemp}>01:00 pm</p>
+                    <p className={styles.hourandtemp}>21º</p>
+                </section>
+
+                <section className={styles.forecastModule}>
+                    <img className={styles.weatherImage} src={imageCloudy}></img>
+                    <p className={styles.hourandtemp}>01:00 pm</p>
+                    <p className={styles.hourandtemp}>21º</p>
+                </section>
+                
+            </section>
+        </div>
+    )
+}
+
+export default Forecast;
