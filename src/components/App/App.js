f04a345938b65757b8cfb72b32aa294c9e7afd7d
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-05-20 18:37:44 -0300

    iniciando na logica

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,22 @@
+import React, {useState} from 'react';
 import styles from './App.module.css'
 import Search from '../Search/search';
 import CurrentWeather from '../CurrentWeather/currentweather';
 import Forecast from '../Forecast/forecast';
+import imageStorm from '../storm.png';
 
 function App() {
+  const [currentLocation, setLocation] = useState([
+    {
+      location: "Rio de Janeiro",
+      temperature: "15º",
+      image: {imageStorm},
+    },
+  ]);
+
   return (
     <div className="App">
-      <Search />
+      <Search currentLocation={currentLocation} />
       <CurrentWeather />
       <Forecast />
     </div>
